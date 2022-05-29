3e5fc6a6fdb5b9a0a4e1959cbcd64e4f80d33706
Natalia Shestakovich <natalia.shestakovich@gmail.com>
2022-05-29 15:52:01 +0300

    Add project files.

---
+++ Task_1st.DaysOfTheWeek/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Task_1st.DaysOfTheWeek
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte userInput = ReadingInput();
+
+            Console.WriteLine($"The {userInput} day of the week is {GetDayOfTheWeek(userInput)}.");  
+        }
+       
+        static sbyte ReadingInput()
+        {
+            Console.WriteLine("Enter a whole number from 1 to 7");
+
+            sbyte result;
+
+            var isNumber = sbyte.TryParse(Console.ReadLine(), out result);
+
+            if (!isNumber || result < 1 || result > 7)
+            {
+                Console.WriteLine("Invalid value");
+
+                return ReadingInput();
+            }
+            else
+            {
+                Console.Clear();
+
+                return result;
+            }
+        }
+
+        static string GetDayOfTheWeek(sbyte numberOfDay)
+        {
+            switch (numberOfDay)
+            {
+                case 1:
+                    return "Monday";
+                case 2:
+                    return "Tuesday";
+                case 3:
+                    return "Wednesday";
+                case 4:
+                    return "Thursday";
+                case 5:
+                    return "Friday";
+                case 6:
+                    return "Saturday";
+                case 7:
+                    return "Sunday";
+                default:
+                    return String.Empty;
+            }
+        }
+    }
+}
---
+++ Task_1st.MultipleTable/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Task_1st.MultipleTable
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            long output = ReadingInput();
+
+            GetMultipTable(output);
+        }
+        static long ReadingInput()
+        {
+            Console.WriteLine("Enter a whole number to obtain a multiplication table");
+
+            long result;
+
+            var isNumber = long.TryParse(Console.ReadLine(), out result);
+
+            if (!isNumber)
+            {
+                Console.WriteLine("Invalid value");
+
+                return ReadingInput();
+            }
+            else
+            {
+                Console.Clear();
+
+                return result;
+            }
+        }
+
+        static void GetMultipTable(long userImput)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                try
+                {
+                    checked
+                    {
+                        Console.WriteLine($"{userImput} x {i} = {userImput * i}");
+                    }
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"The result of multiplication by {i} goes beyond the values of long");
+                }
+
+            }
+        }
+    }
+}
